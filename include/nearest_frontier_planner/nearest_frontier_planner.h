ac6d1761d6f01cc830ffb803dbbf5194bef28005
tyuownu <tyuownu@gmail.com>
2017-12-28 16:20:38 +0800

    style(adding nearest_frontier_planner files)

---
+++ include/nearest_frontier_planner/nearest_frontier_planner.h
@@ -0,0 +1,25 @@
+#ifndef NEAREST_FRONTIER_PLANNER_H_
+#define NEAREST_FRONTIER_PLANNER_H_
+
+#define EXPL_TARGET_SET 1
+#define EXPL_FINISHED   2
+#define EXPL_WAITING    3
+#define EXPL_FAILED     4
+
+#include <nearest_frontier_planner/grid_map.h>
+
+
+class NearestFrontierPlanner
+{
+  public:
+    NearestFrontierPlanner() {}
+    ~NearestFrontierPlanner() {}
+
+    int findExplorationTarget(GridMap* map, unsigned int start,
+        unsigned int &goal);
+
+  private:
+
+};
+
+#endif  // NEAREST_FRONTIER_PLANNER_H_
